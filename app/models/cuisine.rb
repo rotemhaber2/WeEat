74422cf309933153a39adb7d956f4ee933560b6b
rotem.haber <haber.rotem@gmail.com>
2018-02-12 19:02:35 +0200

    add cuisine model

---
+++ app/models/cuisine.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: cuisines
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  icon       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Cuisine < ApplicationRecord
+end
---
+++ test/models/cuisine_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: cuisines
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  icon       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class CuisineTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
