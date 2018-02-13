3928fd773f0f50afd724332b8455036da899f14c
rotem.haber <haber.rotem@gmail.com>
2018-02-13 16:51:50 +0200

    add restaurant CRUD

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,6 +14,14 @@
 
 class Restaurant < ApplicationRecord
 
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :cuisine_id
+
+  validates_length_of :name, :maximum => 70
+  validates_length_of :address, :maximum => 140
+  validates_length_of :cuisine_id, :maximum => 70
+
   has_many :reviews
   has_and_belongs_to_many :cuisines
 
---
+++ db/schema.rb
@@ -0,0 +1,57 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180213133143) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.boolean "accepts_10_bis"
+    t.integer "cuisine_id"
+    t.boolean "kosher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.string "review"
+    t.integer "stars"
+    t.boolean "recommend"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user"
+    t.string "password"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.boolean "is_admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
